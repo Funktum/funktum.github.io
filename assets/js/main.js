b0ae067d46ab55ca2549aa277d8772c6e4258783
Mathias Lindholm <mathias.lindholm@gmail.com>
2017-10-04 21:33:43 +0300

    Refactor image grid
    Use flexbox and support no javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,32 +3,28 @@ jQuery('a[href^="http:"], a[href^="https:"]').not('[href*="funktum.fi"]').attr('
 jQuery('a[href$=".pdf"]').attr('target','_blank');
 
 // Add class to paragraphs containing images
-$('p > img:not([class]').parent('p').attr('class', 'has-image');
+$('p > img:not([class])').parent('p').attr('class', 'image-grid');
 
 // Add lightbox only on bigger screens that are not touch screens
 if ($(window).width() > 600 && !Modernizr.touch) {
-  $('.page-content img:not([class])').attr('class', 'image-popup');
+  $('.image-grid img:not([class])').attr('class', 'image-popup');
 }
 
 // Lightbox
 jQuery(document).ready(function($) {
   $('img.image-popup').click(function(e) {
     e.preventDefault();
+    $('body').toggleClass('no-scroll');
     var image_href = $(this).attr("src");
-
-    // If the lightbox window HTML already exists in document,
-    // change the img src to to match the href of whatever link was clicked
-
     if ($('#lightbox').length > 0) { // #lightbox exists
-      $('#content').html('<img src="' + image_href + '" />');
-      $('#lightbox').fadeIn(200);
+      $('#lightbox').fadeIn(100);
+      $('#content').html('<img src="' + image_href + '" />').delay(100).fadeIn(100);
     }
-
   });
 
-  //Click anywhere on the page to get rid of lightbox window
   $('#lightbox').on('click', function() {
-    $('#lightbox').fadeOut(200);
+    $('body').toggleClass('no-scroll');
+    $('#content').fadeOut(100)
+    $('#lightbox').delay(100).fadeOut(100);
   });
-
 });
